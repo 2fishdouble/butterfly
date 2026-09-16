d54dbd5b1f32d938c13576a82aa6bf9b4e3782f6
dengchaojun996 <cjd0655@gmail.com>
2026-09-16 11:32:07 +0800

    test(sandbox): 移除过时的测试方法
    - 删除了 contextLoads 测试方法及其相关依赖导入
    - 移除了 mailTest 测试方法及邮件附件发送功能测试
    - 清理了未使用的 Java 类导入语句
    - 简化了测试类结构，移除了不必要的测试代码

--- butterfly-sandbox/src/test/java/io/github/butterfly/sandbox/ButterflySandboxApplicationTests.java
+++ butterfly-sandbox/src/test/java/io/github/butterfly/sandbox/ButterflySandboxApplicationTests.java
@@ -20,16 +20,9 @@ import io.github.butterfly.mail.autoconfigure.MailTemplate;
 import io.github.butterfly.sandbox.model.Computer;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.RedisTemplate;
-
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Map;
 
 @SpringBootTest
 class ButterflySandboxApplicationTests {
@@ -52,30 +45,6 @@ class ButterflySandboxApplicationTests {
 
 	@AfterEach
 	void tearDown() {
-	}
-
-	@Test
-	void contextLoads() {
-		Computer computer = new Computer();
-		computer.setId(614999623461548032L);
-		computer.setCreateTime(LocalDateTime.now());
-		computer.setProducts(List.of(new Computer.Product() {
-			{
-				setId(614999623461548033L);
-				setName("product");
-				setPrice(new BigDecimal("1.23695"));
-				setQuantity(1);
-				setTotalAmount(BigDecimal.ONE);
-				setDescription("description");
-			}
-		}));
-		this.computerRedisTemplate.opsForValue().set("computer:1", computer);
-	}
-
-	@Test
-	void mailTest() {
-		this.mailTemplate.sendAttachment("cjd0655@gmail.com", "测试邮件", "这是一封测试邮件",
-				Map.of("README.md", new ClassPathResource("README.md")));
 	}
 
 }
